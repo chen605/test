682c63689225db5317556839d2bdcb20b764c00b
chen605 <chow_chen_1996@hotmail.com>
2021-05-11 14:00:11 +0200

    ....

---
+++ src/App.js
@@ -0,0 +1,40 @@
+
+import React from "react";
+
+import HomeScreen from "./components/HomeScreen";
+import MoviePage from "./pages/MoviePage";
+import DiscoverPage from "./pages/DiscoverPage";
+
+import Nav from "./components/Nav";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Footer from "./components/Footer";
+
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <div className="content-wrap">
+        <Router>
+          <Nav />
+          <Switch>
+            <Route exact path="/">
+              <HomeScreen />
+            </Route>
+            <Route path="/film">
+              <MoviePage />
+            </Route>
+            <Route path="/discover">
+              <DiscoverPage />
+            </Route>
+          </Switch>
+        </Router>
+      </div>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
